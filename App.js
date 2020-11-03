da6efb768c4108e577b8375a6a95093d2fe6026a
ngankhanh98 <ngankhanh98@gmail.com>
2020-11-03 20:40:54 +0700

    🔧 web pack + babel cconfig

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry, Layout } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import React from 'react';
+import TextInputFields from './components/TextInputFields';
+import { INPUT_TYPE } from './contants';
 import Login from './screens/Login/Login';
 
 export default function App() {
+
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={eva.light}>
         <Layout>
-          Hello
-          {/* <Login /> */}
+          <Login />
         </Layout>
       </ApplicationProvider>
     </>
