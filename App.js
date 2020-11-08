5064225074b483575fef009cff510377a96efed0
ngankhanh98 <ngankhanh98@gmail.com>
2020-11-08 20:25:31 +0700

    (+) screens

--- App.js
+++ App.js
@@ -1,33 +1,93 @@
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry, Layout } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+/*!
+
+ =========================================================
+ * Material Kit React Native - v1.4.0
+ =========================================================
+ * Product Page: https://demos.creative-tim.com/material-kit-react-native/
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/material-kit-react-native/blob/master/LICENSE)
+ =========================================================
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import ForgotPassword from './screens/ForgotPassword/ForgotPassword';
-import Login from './screens/Login/Login';
-import Register from './screens/Register/Register';
+import { Platform, StatusBar, Image } from 'react-native';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import { Block, GalioProvider } from 'galio-framework';
 
+import { Images, products, materialTheme } from './constants/';
 
-export default function App() {
-  return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Layout style={styles.row}>
-          {/* <Login /> */}
-          <Register />
-          {/* <ForgotPassword /> */}
-        </Layout>
-      </ApplicationProvider>
-    </>
-  );
+import { NavigationContainer } from '@react-navigation/native';
+import Screens from './navigation/Screens';
+
+// Before rendering any navigation stack
+import { enableScreens } from 'react-native-screens';
+enableScreens();
+
+// cache app images
+const assetImages = [
+  Images.Pro,
+  Images.Profile,
+  Images.Avatar,
+  Images.Onboarding,
+];
+
+// cache product images
+products.map(product => assetImages.push(product.image));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
-const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 30
-  },
-})
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <NavigationContainer>
+          <GalioProvider theme={materialTheme}>
+            <Block flex>
+              {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+              <Screens />
+            </Block>
+          </GalioProvider>
+        </NavigationContainer>
+      );
+    }
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      ...cacheImages(assetImages),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
